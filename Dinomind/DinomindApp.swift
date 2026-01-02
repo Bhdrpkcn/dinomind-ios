8388d2fdf11abc0967cc9b06a4c1f81a9ffd3562
Bhdrpkcn <bhdrpkcn@gmail.com>
2026-01-02 21:27:57 +0300

    Init commit

---
+++ Dinomind/DinomindApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinomindApp.swift
+//  Dinomind
+//
+//  Created by Bahadır Pekcan on 2.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct DinomindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
